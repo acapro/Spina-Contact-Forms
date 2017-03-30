e89d1814140595c4fb7ba46dd813664b4f482712
Adam Cooper <mail@adamcooper.de>
2017-03-30 12:51:10 +0200

    Fixes email addresses

--- app/mailers/spina/contact_form_mailer.rb
+++ app/mailers/spina/contact_form_mailer.rb
@@ -1,11 +1,11 @@
 module Spina
   class ContactFormMailer < ActionMailer::Base
-    default from: Rails.application.secrets
+    default from: Rails.application.secrets.contact_form_from_email
     layout 'mailers/spina_contact_form'
 
     def send_form(email_content)
       @email_content = email_content
-      mail(to: 'adam@adamcooper.de', subject: 'From your Contact Form')
+      mail(to: current_account.email, subject: 'From your Contact Form')
     end
   end
 end
